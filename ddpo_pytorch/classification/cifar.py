9bf76bdd1747d9ec32296fb8b2191c8678218915
GiilDe <gilad.deutch@gmail.com>
2023-08-17 13:46:28 +0300

    cifar fix

--- ddpo_pytorch/classification/cifar.py
+++ ddpo_pytorch/classification/cifar.py
@@ -1,3 +1,4 @@
+from typing import Any
 import torch
 import torchvision
 from torchvision.models import efficientnet, EfficientNet_V2_S_Weights
@@ -6,5 +7,20 @@ from torch import nn
 
 from ddpo_pytorch.classification.classifier import Classifier
 from transformers import pipeline
+from transformers.pipelines.image_classification import ImageClassificationPipeline
 
-pipe = pipeline("image-classification", model="tzhao3/vit-CIFAR10")
+
+class CifarClassifier(Classifier):
+    def __init__(self) -> None:
+        self.pipe: ImageClassificationPipeline = pipeline(
+            "image-classification", model="tzhao3/vit-CIFAR10"
+        )
+        self.pipe.model.eval()
+
+    def preprocess(self, pil_image) -> torch.Tensor:
+        return self.pipe.preprocess(pil_image)["pixel_values"].squeeze(0)
+
+    def predict(self, tensor_images) -> torch.Tensor:
+        outputs = self.pipe.model(tensor_images)
+        probs = outputs.logits.softmax(-1)
+        return probs
--- ddpo_pytorch/classification/factory.py
+++ ddpo_pytorch/classification/factory.py
@@ -1,13 +1,13 @@
-from ddpo_pytorch.classification.cifar import pipe
+from ddpo_pytorch.classification.cifar import CifarClassifier
 from ddpo_pytorch.classification.mnist import MnistClassifier
 
 
 def init_by_dataset(dataset_name):
     if dataset_name == "MNIST":
-      return MnistClassifier()
-    
+        return MnistClassifier()
+
     if dataset_name == "CIFAR10":
-      return pipe
-    
+        return CifarClassifier()
+
     else:
-      raise ValueError("Unrecognized dataset name.")
+        raise ValueError("Unrecognized dataset name.")
