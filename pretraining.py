d02680a72858d6652487219495a551648d1386af
GiilDe <gilad.deutch@gmail.com>
2023-08-29 16:05:13 +0300

    .

--- pretraining.py
+++ pretraining.py
@@ -1,26 +1,32 @@
 from dataclasses import dataclass
+import dataclasses
 from datasets import load_dataset
 from torchvision import transforms
 import torch
-from diffusers import DDPMPipeline
+from diffusers import DDPMPipeline, DDIMPipeline
 import torch.nn.functional as F
 from diffusers.optimization import get_cosine_schedule_with_warmup
 
 from PIL import Image
-from diffusers import DDPMScheduler
+from diffusers import DDPMScheduler, DDIMScheduler
 from diffusers import UNet2DModel
-from pretrain_model import MnistClassifier
+from pretraining_utils import MnistClassifier, PipelineIterator, evaluate_diff
 from art.utils import load_mnist
 from art.attacks.evasion import FastGradientMethod
 from art.estimators.classification import PyTorchClassifier
-from ddpo_pytorch.rewards import l2_norm_diff, l_inf_norm_diff
 import os
+import logging
+import wandb
+from accelerate import Accelerator
+from tqdm.auto import tqdm
+
+logging.basicConfig(format="%(message)s", level=logging.INFO)
 
 
 @dataclass
 class TrainingConfig:
     train_batch_size = 16
-    eval_batch_size = 16
+    eval_batch_size = 32
     num_epochs = 1
     gradient_accumulation_steps = 1
     learning_rate = 1e-4
@@ -31,6 +37,17 @@ class TrainingConfig:
     output_dir = "diffusion_testing"
     seed = 0
     l_inf_noise = 0
+    add_pertruebation = True
+    num_inference_steps = 50
+    diffusion_class = DDIMPipeline
+    scheduler_class = DDIMScheduler
+    save_images_to_disk = False
+    run_name = "adv pertrubation test, training_steps = 1000"
+    training_steps = 1000
+    data_from_model = False
+
+    def to_dict(self) -> dict:
+        return dataclasses.asdict(self)
 
 
 config = TrainingConfig()
@@ -39,13 +56,20 @@ config = TrainingConfig()
 config.dataset_name = "mnist"
 dataset = load_dataset(config.dataset_name, split="train")
 
+pipeline_train = config.diffusion_class.from_pretrained("nabdan/mnist_20_epoch")
+pipeline_train.scheduler.set_timesteps(config.num_inference_steps)
+model: UNet2DModel = pipeline_train.unet
 
-preprocess = transforms.Compose(
-    [
-        transforms.ToTensor(),
-        transforms.Normalize([0.5], [0.5]),
-    ]
+model_id = "nabdan/mnist_20_epoch"
+noise_scheduler = config.scheduler_class.from_pretrained(
+    model_id, num_train_timesteps=config.training_steps
 )
+noise_scheduler.set_timesteps(config.num_inference_steps)
+
+pipeline_frozen = DDPMPipeline.from_pretrained(model_id)
+pipeline_frozen.scheduler = config.scheduler_class.from_config(model_id)
+pipeline_frozen.scheduler.set_timesteps(config.num_inference_steps)
+
 
 to_tensor = transforms.ToTensor()
 normalize = transforms.Normalize([0.5], [0.5])
@@ -58,18 +82,11 @@ def transform(examples):
 
 dataset.set_transform(transform)
 
-
 train_dataloader = torch.utils.data.DataLoader(
     dataset, batch_size=config.train_batch_size, shuffle=True
 )
-
-
-pipeline_orig = DDPMPipeline.from_pretrained("nabdan/mnist_20_epoch")
-model: UNet2DModel = pipeline_orig.unet
-
-
-# noise_scheduler_ = DDPMScheduler(num_train_timesteps=1000)
-noise_scheduler = DDPMScheduler.from_pretrained("nabdan/mnist_20_epoch")
+if config.data_from_model:
+    train_dataloader = PipelineIterator(pipeline_frozen, config.train_batch_size, length=len(train_dataloader))
 
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
@@ -88,40 +105,47 @@ def make_grid(images, rows, cols):
     return grid
 
 
-def evaluate(config, epoch, pipeline):
+def evaluate(config, epoch, pipeline, postfix=""):
+    logging.info(f"Generating samples for epoch {epoch}, {postfix}")
+    logging.info(f"num inference steps: {pipeline.scheduler.num_inference_steps}")
     images = pipeline(
         batch_size=config.eval_batch_size,
         generator=torch.manual_seed(config.seed),
+        num_inference_steps=pipeline.scheduler.num_inference_steps,
     ).images
 
-    image_grid = make_grid(images, rows=4, cols=4)
+    if config.save_images_to_disk:
+        image_grid = make_grid(images, rows=4, cols=4)
 
-    test_dir = os.path.join(config.output_dir, "samples")
-    os.makedirs(test_dir, exist_ok=True)
-    image_grid.save(f"{test_dir}/{epoch:04d}.png")
+        test_dir = os.path.join(config.output_dir, f"samples")
+        os.makedirs(test_dir, exist_ok=True)
+        image_grid.save(
+            f"{test_dir}/{epoch:04d}.png"
+            if not postfix
+            else f"{test_dir}/{epoch:04d}_{postfix}.png"
+        )
+    return images
 
 
-from accelerate import Accelerator
-from tqdm.auto import tqdm
-import os
-
-if isinstance(pipeline_orig.unet.config.sample_size, int):
+if isinstance(model.config.sample_size, int):
     image_shape = (
         config.train_batch_size,
-        pipeline_orig.unet.config.in_channels,
-        pipeline_orig.unet.config.sample_size,
-        pipeline_orig.unet.config.sample_size,
+        model.config.in_channels,
+        model.config.sample_size,
+        model.config.sample_size,
     )
 else:
     image_shape = (
         config.train_batch_size,
-        pipeline_orig.unet.config.in_channels,
-        *pipeline_orig.unet.config.sample_size,
+        model.config.in_channels,
+        *model.config.sample_size,
     )
+
 _, _, min_pixel_value, max_pixel_value = load_mnist()
 criterion = torch.nn.CrossEntropyLoss()
-classifier = PyTorchClassifier(
-    model=MnistClassifier(),
+classifier = MnistClassifier()
+classifier_attack = PyTorchClassifier(
+    model=classifier,
     clip_values=(min_pixel_value, max_pixel_value),
     loss=criterion,
     optimizer=optimizer,
@@ -129,7 +153,18 @@ classifier = PyTorchClassifier(
     nb_classes=10,
 )
 attack = FastGradientMethod(
-    estimator=classifier, eps=0.3, eps_step=0.1, batch_size=config.train_batch_size
+    estimator=classifier_attack,
+    eps=0.3,
+    eps_step=0.1,
+    batch_size=config.train_batch_size,
+)
+
+
+original_images = evaluate(
+    config,
+    0,
+    pipeline_frozen,
+    postfix="orig",
 )
 
 
@@ -139,20 +174,34 @@ def train_loop(
     accelerator = Accelerator(
         mixed_precision=config.mixed_precision,
         gradient_accumulation_steps=config.gradient_accumulation_steps,
-        log_with="tensorboard",
+        log_with="wandb",
         project_dir=os.path.join(config.output_dir, "logs"),
+    )
+    accelerator.init_trackers(
+        project_name="ddpo-pytorch-pretraining",
+        config=config.to_dict(),
+        init_kwargs={"wandb": {"name": config.run_name}},
     )
     if accelerator.is_main_process:
         if config.output_dir is not None:
             os.makedirs(config.output_dir, exist_ok=True)
         accelerator.init_trackers("train_example")
 
+    global_step = 0
+
+    accelerator.log(
+        {
+            "original images": [wandb.Image(image) for image in original_images],
+        },
+        step=global_step,
+    )
+
     model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
         model, optimizer, train_dataloader, lr_scheduler
     )
 
-    global_step = 0
-
+    # to use same noise for the trained and frozen models.
+    generator = torch.Generator()
     for epoch in range(config.num_epochs):
         progress_bar = tqdm(
             total=len(train_dataloader), disable=not accelerator.is_local_main_process
@@ -160,29 +209,36 @@ def train_loop(
         progress_bar.set_description(f"Epoch {epoch}")
 
         for step, batch in enumerate(train_dataloader):
-            clean_images = batch["images"]
+            clean_images = batch["images"] if not config.data_from_model else batch[0]
             device = clean_images.device
             if config.l_inf_noise > 0:
                 clean_images += torch.rand_like(clean_images) * config.l_inf_noise
 
-            adversrial_images = attack.generate(clean_images.cpu().numpy())
-            adversrial_images = torch.from_numpy(adversrial_images).to(device)
+            if config.add_pertruebation:
+                adversrial_images = attack.generate(clean_images.cpu().numpy())
+                adversrial_images = torch.from_numpy(adversrial_images).to(device)
+            else:
+                adversrial_images = clean_images
 
-            adversrial_images = normalize(adversrial_images)
+            target_images = normalize(adversrial_images)
 
-            noise = torch.randn(adversrial_images.shape).to(device)
-            bs = adversrial_images.shape[0]
+            generator = (
+                torch.Generator().set_state(batch[1])
+                if config.data_from_model
+                else None
+            )
+            noise = torch.randn(target_images.shape, generator=generator).to(device)
+            bs = target_images.shape[0]
 
+            logging.info(f"num train timesteps: {config.training_steps}")
             timesteps = torch.randint(
                 0,
-                noise_scheduler.config.num_train_timesteps,
+                config.training_steps,
                 (bs,),
                 device=device,
             ).long()
 
-            noisy_images = noise_scheduler.add_noise(
-                adversrial_images, noise, timesteps
-            )
+            noisy_images = noise_scheduler.add_noise(target_images, noise, timesteps)
 
             with accelerator.accumulate(model):
                 noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
@@ -212,12 +268,20 @@ def train_loop(
             if (
                 epoch + 1
             ) % config.save_image_epochs == 0 or epoch == config.num_epochs - 1:
-                evaluate(config, epoch, pipeline)
+                images_predicted = evaluate(config, epoch, pipeline)
+                evaluate_diff(
+                    images_predicted,
+                    original_images,
+                    device,
+                    accelerator,
+                    global_step,
+                    classifier,
+                )
 
-            if (
-                epoch + 1
-            ) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1:
-                pipeline.save_pretrained(config.output_dir)
+            # if (
+            #     epoch + 1
+            # ) % config.save_model_epochs == 0 or epoch == config.num_epochs - 1:
+            # pipeline_train.save_pretrained(config.output_dir)
 
 
 from accelerate import notebook_launcher
---
+++ pretraining_utils.py
@@ -0,0 +1,134 @@
+## Training configuration
+
+# For convenience, create a `TrainingConfig` class containing the training hyperparameters (feel free to adjust them):
+
+import logging
+import torch.nn.functional as F
+import wandb
+
+import torch
+
+import os
+import torchvision
+
+import torch
+from ddpo_pytorch.rewards import l2_norm_diff, l_inf_norm_diff
+import torch.nn as nn
+from torchvision import transforms
+
+
+cuda = torch.device("cuda")
+
+
+class ConvNetClassifier(nn.Module):
+    def __init__(self):
+        super(ConvNetClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return x
+
+
+class MnistClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self._net = ConvNetClassifier()
+        self._net.load_state_dict(torch.load("model.pth"))
+        self._net.eval()
+        self._net.to("cuda")
+        self._transforms_tensor = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        )
+        self._transforms = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        )
+
+    def preprocess(self, x):
+        if not isinstance(x, torch.Tensor):
+            return torch.stack([self._transforms_tensor(image) for image in x]).to(cuda)
+
+        return self._transforms(x)
+        return x
+
+    def forward(self, x):
+        pred = self._net(x)
+        return pred
+
+
+def evaluate_diff(
+    images_predicted, original_images, device, accelerator, global_step, classifier
+):
+    with torch.no_grad():
+        l2_diff = l2_norm_diff(images_predicted, original_images, device=device)
+        l_inf_diff = l_inf_norm_diff(images_predicted, original_images, device=device)
+        logging.info(f"l2_diff: {l2_diff}")
+        logging.info(f"l_inf_diff: {l_inf_diff}")
+        logging.info(f"l2_diff mean: {l2_diff.mean().item()}")
+        logging.info(f"l_inf_diff mean: {l_inf_diff.mean().item()}")
+
+        original_scores = classifier.forward(classifier.preprocess(original_images))
+        labels = original_scores.argmax(dim=1)
+
+        ft_scores = classifier.forward(classifier.preprocess(images_predicted))
+        ft_labels = ft_scores.argmax(dim=1)
+        accuracy = (ft_labels == labels).float().mean()
+        logging.info(f"accuracy: {accuracy.item()}")
+
+        accelerator.log(
+            {
+                "l2_diff": l2_diff.mean().item(),
+                "l_inf_diff": l_inf_diff.mean().item(),
+                "step": global_step,
+                "l_inf_diff_hist": l_inf_diff.flatten(),
+                "l2_diff_hist": l2_diff.flatten(),
+                "accuracy": accuracy.item(),
+            },
+            step=global_step,
+        )
+
+        accelerator.log(
+            {
+                "predicted pertrubed images": [
+                    wandb.Image(image) for image in images_predicted
+                ],
+            },
+            step=global_step,
+        )
+
+
+to_tensor = transforms.ToTensor()
+
+
+class PipelineIterator:
+    def __init__(self, pipeline, batch_size, length):
+        self._pipeline = pipeline
+        self._batch_size = batch_size
+        self.generator = torch.Generator()
+        self.length = length
+
+    def __len__(self):
+        return self.length
+
+    def __next__(self):
+        generator_state = self.generator.get_state()
+        images = self._pipeline(
+            batch_size=self._batch_size,
+            generator=self.generator,
+            num_inference_steps=self._pipeline.scheduler.num_inference_steps,
+        ).images
+        return to_tensor(images), generator_state
